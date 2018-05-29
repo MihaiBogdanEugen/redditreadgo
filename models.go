2fecb20603973c6e9a8116af32ec1ba9b5ec072a
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2018-05-30 01:14:45 +0200

    More comments

--- models.go
+++ models.go
@@ -5,12 +5,17 @@ type PopularitySort string
 
 const (
 	// DefaultPopularity
-	DefaultPopularity        PopularitySort = ""
-	HotSubmissions                          = "hot"
-	NewSubmissions                          = "new"
-	RisingSubmissions                       = "rising"
-	TopSubmissions                          = "top"
-	ControversialSubmissions                = "controversial"
+	DefaultPopularity PopularitySort = ""
+	// HotSubmissions
+	HotSubmissions = "hot"
+	// NewSubmissions
+	NewSubmissions = "new"
+	// RisingSubmissions
+	RisingSubmissions = "rising"
+	// TopSubmissions
+	TopSubmissions = "top"
+	// ControversialSubmissions
+	ControversialSubmissions = "controversial"
 )
 
 // AgeSort represents the possible ways to sort submissions by age.
@@ -18,10 +23,15 @@ type AgeSort string
 
 const (
 	// ThisHour
-	ThisHour  AgeSort = "hour"
-	ThisDay           = "day"
-	ThisWeek          = "week"
-	ThisMonth         = "month"
-	ThisYear          = "year"
-	AllTime           = "all"
+	ThisHour AgeSort = "hour"
+	// ThisDay
+	ThisDay = "day"
+	// ThisWeek
+	ThisWeek = "week"
+	//ThisMonth
+	ThisMonth = "month"
+	// ThisYear
+	ThisYear = "year"
+	// AllTime
+	AllTime = "all"
 )
