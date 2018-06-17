01834d0ec39403baf5c42f45f1262eb466ae13f4
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2018-06-17 13:37:42 +0200

    Fix models and types, previously mixed

--- types.go
+++ types.go
@@ -1,28 +1,37 @@
 package redditreadgo
 
-// Submission represents an individual post from the perspective of a subreddit
-type Submission struct {
-	Author        string  `json:"author"`
-	Title         string  `json:"title"`
-	URL           string  `json:"url"`
-	Domain        string  `json:"domain"`
-	Subreddit     string  `json:"subreddit"`
-	SubredditID   string  `json:"subreddit_id"`
-	FullID        string  `json:"name"`
-	ID            string  `json:"id"`
-	Permalink     string  `json:"permalink"`
-	Selftext      string  `json:"selftext"`
-	SelftextHTML  string  `json:"selftext_html"`
-	ThumbnailURL  string  `json:"thumbnail"`
-	DateCreated   float64 `json:"created_utc"`
-	NumComments   int     `json:"num_comments"`
-	Score         int     `json:"score"`
-	Ups           int     `json:"ups"`
-	Downs         int     `json:"downs"`
-	IsNSFW        bool    `json:"over_18"`
-	IsSelf        bool    `json:"is_self"`
-	WasClicked    bool    `json:"clicked"`
-	IsSaved       bool    `json:"saved"`
-	BannedBy      *string `json:"banned_by"`
-	LinkFlairText string  `json:"link_flair_text"`
-}
+// PopularitySort represents the possible ways to sort submissions by popularity.
+type PopularitySort string
+
+const (
+	// DefaultPopularity value
+	DefaultPopularity PopularitySort = ""
+	// HotSubmissions value
+	HotSubmissions = "hot"
+	// NewSubmissions value
+	NewSubmissions = "new"
+	// RisingSubmissions value
+	RisingSubmissions = "rising"
+	// TopSubmissions value
+	TopSubmissions = "top"
+	// ControversialSubmissions value
+	ControversialSubmissions = "controversial"
+)
+
+// AgeSort represents the possible ways to sort submissions by age.
+type AgeSort string
+
+const (
+	// ThisHour value
+	ThisHour AgeSort = "hour"
+	// ThisDay value
+	ThisDay = "day"
+	// ThisWeek value
+	ThisWeek = "week"
+	//ThisMonth value
+	ThisMonth = "month"
+	// ThisYear value
+	ThisYear = "year"
+	// AllTime value
+	AllTime = "all"
+)
